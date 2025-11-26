f247f5f3c51b2b0786669f06e95699b8744f9d76
onikatashi <llho717@naver.com>
2025-11-26 13:13:50 +0900

    이동 스크립트 테스트
    - Pull 당겼을 때, 작업하던 거에서 추가되는지 확인

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+        // 상하 이동 장운이형
+
+        // 좌우 이동 의현이
+    }
+}
