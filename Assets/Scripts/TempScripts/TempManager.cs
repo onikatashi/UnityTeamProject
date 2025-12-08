806c96e98993ae2133c8f1581b5a495aca50821c
onikatashi <llho717@naver.com>
2025-12-08 16:03:14 +0900

    인벤토리 중복 아이템 획득 불가 기능
    - 인벤토리에 같은 ID를 가진 아이템을 획득하지 못하게 함
    - UICheckScene에서 0 ~ 9, Q, W 키로 아이템 획득 확인 가능

--- Assets/Scripts/TempScripts/TempManager.cs
+++ Assets/Scripts/TempScripts/TempManager.cs
@@ -12,12 +12,8 @@ public class TempManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Q))
-        {
-            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.CreateItemInstance(1));
-            InventoryManager.Instance.GetInventoryTotalStats();
-        }
-        
+        testGetItem();
+
         if(Input.GetKeyDown(KeyCode.I))
         {
             UIManager.Instance.ToggleInventory();
@@ -27,6 +23,58 @@ public class TempManager : MonoBehaviour
         {
             int randIndex = UnityEngine.Random.Range(0, InventoryManager.Instance.Inventory.Length);
             InventoryManager.Instance.ReinforceInventorySlot(randIndex);
+        }
+    }
+
+    void testGetItem()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(1));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(2));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(3));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(4));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(5));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha6))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(6));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(7));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha8))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(8));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha9))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(9));
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(10));
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(11));
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            InventoryManager.Instance.AddItemToInventory(ItemManager.Instance.GetItemData(12));
         }
     }
 }
