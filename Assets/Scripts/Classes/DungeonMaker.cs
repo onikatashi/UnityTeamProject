f9d3be47f0a189aa3e497305d21802daa51caabf
kimjangw <kimjangw95@gmail.com>
2025-12-08 10:30:54 +0900

    던전 확률 관련 중간 저장
    - DungeonMap 형상 구성
    - 방 SO 구성(방 타입별 확률)

--- Assets/Scripts/Classes/DungeonMaker.cs
+++ Assets/Scripts/Classes/DungeonMaker.cs
@@ -5,10 +5,14 @@ using UnityEngine;
 /// </summary>
 public class DungeonMaker : MonoBehaviour
 {
+    //테스트 중
+    public RoomTypeData roomData;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        //Debug.Log("Room Type: " + roomData.roomType);
+        //Debug.Log("Probability: " + roomData.RoomTypeProbabilities);
     }
 
     // Update is called once per frame
--- Assets/Scripts/ScriptableObjects/RoomTypeData.cs
+++ Assets/Scripts/ScriptableObjects/RoomTypeData.cs
@@ -5,8 +5,55 @@ using UnityEngine;
 /// 1. 던전 방 타입 확률을 저장하는 SO
 /// 2. 방 타입, 등장 확률 등을 포함
 /// </summary>
+
+    // 방(노드) 종류
+    //public enum RoomType
+    //{
+    //    Normal,
+    //    Elite,
+    //    Shop,
+    //    Rest,
+    //    Forge,
+    //    Boss
+    //}
+
 [CreateAssetMenu(fileName = "RoomTypeData", menuName = "Scriptable Objects/RoomTypeData")]
 public class RoomTypeData : ScriptableObject
 {
-    public Dictionary<Enums.RoomType, float> roomTypeProbabilities;     // 방 타입별 등장 확률
+
+
+    [System.Serializable]
+    public class RoomProbability
+    {
+        public Enums.RoomType roomType;
+
+        [Range(0f, 100f)]
+        public float probability;
+    }
+
+    public List<RoomProbability> roomList;
+
+
+
+    //// Key: RoomType, Value: 등장 확률
+    //public Dictionary<Enums.RoomType, float> roomTypeProbabilities = new Dictionary<Enums.RoomType, float>();
+
+
+    //private void Awake()
+    //{
+    //    //딕셔너리 몬스터 던전 관련
+    //    roomTypeProbabilities.Add(Enums.RoomType.Normal, 30f);
+    //    roomTypeProbabilities.Add(Enums.RoomType.Elite, 20f);
+    //    roomTypeProbabilities.Add(Enums.RoomType.Boss, 10f);
+
+    //    roomTypeProbabilities.Add(Enums.RoomType.Shop, 10f);
+    //    roomTypeProbabilities.Add(Enums.RoomType.Rest, 20f);
+    //    roomTypeProbabilities.Add(Enums.RoomType.Forge, 10f);
+
+    //}
+
+
+    //public Enums.RoomType roomType;     // 방 타입별 등장 확률
+    
+    //public float RoomTypeProbabilities; // 해당 노드의 등장 확률
 }
