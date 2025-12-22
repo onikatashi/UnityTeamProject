c34d3cc417b13e8aa0a3759d5d06de799e0bce34
Rikan <rikandev@gmail.com>
2025-12-22 17:55:52 +0900

    ForgeManager 업데이트
    3개의 선택지 제공, 1개 선택시 다시 선택 불가능한 로직 추가했습니다.

--- Assets/_Scripts/Managers/ForgeManager.cs
+++ Assets/_Scripts/Managers/ForgeManager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ForgeManager : MonoBehaviour
@@ -6,6 +7,10 @@ public class ForgeManager : MonoBehaviour
 
     [Header("UI References")]
     [SerializeField] private GameObject forgePanel;
+    [SerializeField] private TextMeshPro tipText;
+
+    // 선택 여부
+    public bool isSelected = false;
 
     private void Awake()
     {
@@ -15,12 +20,22 @@ public class ForgeManager : MonoBehaviour
 
     private void Start()
     {
+        // 최초 로드 시 이전 forge 의 선택 여부 초기화
+        isSelected = false;
+
         if (forgePanel != null)
             forgePanel.SetActive(false);
     }
 
     public void OpenForge()
     {
+        // 이미 선택을 끝냈다면 다시 창을 열 수 없음
+        if (isSelected)
+        {
+            Debug.Log("이미 선택 완료됨. Forge UI 열리지 않음.");
+            return;
+        }
+
         if (forgePanel != null)
         {
             forgePanel.SetActive(true);
@@ -36,4 +51,49 @@ public class ForgeManager : MonoBehaviour
             Debug.Log("대장간 UI 비활성화");
         }
     }
+
+    // ---------------------
+    // 선택 버튼들 (추후 구현 예정)
+    // ---------------------
+
+    public void Select1()
+    {
+        CompleteSelection();
+
+        // 선택1 기능 구현
+        Debug.Log("Select1 눌림");
+
+        CloseForge();
+    }
+
+    public void Select2()
+    {
+        CompleteSelection();
+
+        // 선택2 기능 구현
+        Debug.Log("Select2 눌림");
+
+        CloseForge();
+    }
+
+    public void Select3()
+    {
+        CompleteSelection();
+
+        // 선택3 기능 구현
+        Debug.Log("Select3 눌림");
+
+        CloseForge();
+    }
+
+    private void CompleteSelection()
+    {
+        isSelected = true; //선택 완료
+
+        if (tipText != null) //월드 팁 텍스트 변경
+            tipText.text = "선택 완료됨";
+
+        CloseForge();
+    }
+
 }
