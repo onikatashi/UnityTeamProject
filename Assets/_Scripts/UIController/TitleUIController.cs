f2c66aa9e4830d0142f2767dd444aafdc6bfc71d
onikatashi <llho717@naver.com>
2025-12-26 02:11:49 +0900

    타이틀 화면 버튼 이미지, 사운드 추가

--- Assets/_Scripts/UIController/TitleUIController.cs
+++ Assets/_Scripts/UIController/TitleUIController.cs
@@ -8,11 +8,13 @@ public class TitleUIController : MonoBehaviour
     public Button exitButton;
 
     SceneLoaderManager sceneLoaderManager;
+    SoundManager soundManager;
     UIManager uiManager;
 
     void Start()
     {
         sceneLoaderManager = SceneLoaderManager.Instance;
+        soundManager = SoundManager.Instance;
         uiManager = UIManager.Instance;
 
         startButton.onClick.AddListener(startGame);
@@ -22,16 +24,19 @@ public class TitleUIController : MonoBehaviour
 
     public void startGame()
     {
+        soundManager.PlaySFX("buttonClick");
         sceneLoaderManager.LoadScene(SceneNames.Town);
     }
 
     public void settingButtonClick()
     {
+        soundManager.PlaySFX("buttonClick");
         uiManager.settingUIController.ShowSettingPanel();
     }
 
     public void QuitGame()
     {
+        soundManager.PlaySFX("buttonClick");
         UnityEditor.EditorApplication.isPlaying = false;
     }
 
